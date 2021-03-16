a8ce76a0c63a7867ae5e678969258a9db5a6f198
Mikael <mikael.bergstrom@ga.ntig.se>
2021-03-16 18:26:04 +0100

    Initial commit

---
+++ ApiServer/Controllers/PokemonController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiServer.Controllers
+{
+  [ApiController]
+  [Route("api/pokemon")]
+  public class PokemonController: ControllerBase
+  {
+    List<Pokemon> pokemons = new List<Pokemon>()
+    {
+      new Pokemon() {Name = "Pikachu"},
+      new Pokemon() {Name = "Snorlax"}
+    };
+
+    [HttpGet]
+    public ActionResult Get()
+    {
+      return Ok(pokemons[0]);
+    }
+
+    [HttpPost()]
+    public ActionResult Post(Pokemon pokemon)
+    {
+      // pokemons.Add(pokemon)
+      Console.WriteLine("String: " + pokemon.Name);
+
+      return Ok();
+    }
+  }
+}
